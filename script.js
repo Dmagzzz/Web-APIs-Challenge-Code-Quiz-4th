4f9385ea45b694978f2729955663a1806b1a7679
Dillon Maglio <hddod15@gmail.com>
2022-11-08 22:47:56 -0500

    first one

---
+++ script.js
@@ -0,0 +1,14 @@
+var startButtonEl = document.getElementById("startbtn")
+
+console.log(startButtonEl)
+
+startButtonEl.addEventListener("click", function () {
+    console.log("hello")
+
+
+    var headerEl = document.querySelector(".quiz-intro")
+    headerEl.style.display = "none"
+
+
+    
+})
